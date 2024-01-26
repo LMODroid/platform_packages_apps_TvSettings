LineageOS/android_packages_apps_TvSettings

TvSettings: add Buttons-section to System settings
Change-Id: Iadfbd23b9ca984ce926186205f5a6202780cdb9e

Settings/src/com/android/tv/settings/system/ButtonsActivity.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tv.settings.system;
+
+import android.app.Fragment;
+
+import com.android.tv.settings.TvSettingsActivity;
+import com.android.tv.settings.overlay.FeatureFactory;
+
+public class ButtonsActivity extends TvSettingsActivity {
+
+    @Override
+    protected Fragment createSettingsFragment() {
+        return FeatureFactory.getFactory(this).getSettingsFragmentProvider()
+            .newSettingsFragment(ButtonsFragment.class.getName(), null);
+    }
+}

LMODroid/platform_packages_apps_TvSettings

Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

Settings/src/com/android/tv/settings/system/ButtonsActivity.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tv.settings.system;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.tv.settings.TvSettingsActivity;
+import com.android.tv.settings.overlay.FlavorUtils;
+
+public class ButtonsActivity extends TvSettingsActivity {
+
+    @Override
+    protected Fragment createSettingsFragment() {
+        return FlavorUtils.getFeatureFactory(this).getSettingsFragmentProvider()
+            .newSettingsFragment(ButtonsFragment.class.getName(), null);
+    }
+}